48e28e51e55d4b0b3793bfb66de1a39da05ec154
Henri Brestea <henri.bresteau@gmail.com>
2021-01-14 11:49:12 +0100

    use Layout

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,33 @@
+import Layout from "../components/Layout";
+
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <Layout page="Crypto-watch - About">
+      <h1 className="text-4xl"> About </h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat deleniti
+        animi dolores magni neque dolor nostrum autem ducimus, recusandae,
+        consequatur dicta tenetur minus nobis ratione quibusdam minima totam
+        explicabo. Minus ratione neque unde temporibus quo rerum sit repellendus
+        velit adipisci! Quia optio consequatur, maiores alias cumque sint
+        repellendus quos modi odit rem amet possimus adipisci magnam dolor eum
+        provident soluta neque obcaecati! In non amet explicabo eligendi quasi
+        necessitatibus tempora, laboriosam rerum ipsa fugiat, tempore saepe
+        molestiae corporis, corrupti sit.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+        accusantium. Ipsa explicabo distinctio, veritatis expedita pariatur
+        impedit, id voluptate dolorum non quas ut asperiores minima consequatur
+        quos maiores culpa eaque suscipit debitis quam similique! Ratione soluta
+        magni deleniti nisi cupiditate non et distinctio consequuntur nostrum
+        delectus laborum ipsa reiciendis accusantium fugit, doloremque eligendi
+        deserunt! Commodi, rerum quia earum quasi neque quidem, deleniti labore
+        tempore dolorum ea in omnis pariatur doloremque facere exercitationem
+        minus fuga esse mollitia asperiores nemo similique nisi!
+      </p>
+    </Layout>
+  );
 }
